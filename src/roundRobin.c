e0c02fdd4cbb6e090dd58caa8e0c890afc33314d
Alessandro <alessanbiz@gmail.com>
2021-10-03 21:18:21 -0300

    print to round robin scheduler

--- src/roundRobin.c
+++ src/roundRobin.c
@@ -35,54 +35,88 @@ int chegadaProcesso(Processo* processos, int numeroDeProcessos, int tempo, Proce
 
 void roundRobin(Processo* processos, int numeroDeProcessos, int quantum) {
 		int tempo, qtdNaFila;
+		int quantumOriginal = quantum;
+		float totalDeEspera;
 		Processo *fila;
 		fila = malloc(numeroDeProcessos *sizeof(Processo));
+
+		totalDeEspera = 0;
 		qtdNaFila = 0;
 
 		tempo = duracaoTotal(processos, numeroDeProcessos);
 		qtdNaFila = chegadaProcesso(processos, numeroDeProcessos, 0,fila, qtdNaFila);
-		for (int i = 0; i < tempo; i++) {
-			for (int j = 0; j < qtdNaFila; j++) {
-				if( j==0 ){
-					fila[j].time += 1;
-					quantum -= 1;
-				} else{
-					fila[j].waitingTime += 1;
-				}
-			}
-			//printf("Tempo %d - %d ", i, qtdNaFila);
-			printf("%d = %d - %d - %d - %d - %d\n", i, fila[0].processNumber,fila[1].processNumber,fila[2].processNumber, fila[3].processNumber, fila[4].processNumber);
-			printf("%d - quantidade na fila = %d\n", fila[0].time, qtdNaFila);
+		for (int i = 0; i <= tempo; i++) {
+			quantum -= 1;
 			if ((fila[0].duration - fila[0].time) == 0) {
+				for (int j = 0; j < numeroDeProcessos; j++){
+					if (fila[0].processNumber == processos[j].processNumber){
+						processos[j].waitingTime = fila[0].waitingTime;
+					}
+				}
+				totalDeEspera+= fila[0].waitingTime;
 				pop(qtdNaFila, fila);
 				qtdNaFila -= 1;
-				quantum = 4;
+				quantum = quantumOriginal;
 			}
 			else if (quantum == 0){
 				Processo processoAtual = fila[0];
 				pop(qtdNaFila, fila);
 				push(qtdNaFila-1, fila, processoAtual);
-				quantum = 4;
+				quantum = quantumOriginal;
 			}else{
 				for(int k=0; k<fila[0].qtdeIO; k++){
-						if(atoi(fila[0].io[k]) == fila[0].time){
+						if(atoi((char*)fila[0].io[k]) == fila[0].time){
 							Processo processoAtual = fila[0];
 							pop(qtdNaFila, fila);
 							push(qtdNaFila-1, fila, processoAtual);
-							quantum = 4;
+							quantum = quantumOriginal;
 							break;
 						}
 				}
 			}
-			if (i != 0)
+			if (i != 0){
 				qtdNaFila = chegadaProcesso(processos, numeroDeProcessos, i,fila, qtdNaFila);
+			}
+
+			for (int j = 0; j < qtdNaFila; j++) {
+				if( j == 0 ) {
+					fila[j].time += 1;
+				}else {
+					fila[j].waitingTime += 1;
+				}
+			}
 		}
-	// 	for(int i=0; i< numberOfProcess;i++) {
-	// 	printf("Processo: %d\n", processos[i].processNumber);
-	// 	printf("Qtde IO: %d \n", processos[i].qtdeIO);
-	// 	for(int j=0; j< processos[i].qtdeIO; j++) {
-	// 		printf("IO: %s\n", processos[i].io[j]);
-	// 	}
-	// }
+
+	printf("\nPID.\tDuracao\tChegada\tI/O\n");
+	printf("-----------------------------\n");
+		for(int i=0; i< numeroDeProcessos; i++){
+			printf("P%d\t%d\t%d\t", 
+			processos[i].processNumber, 
+			processos[i].duration,
+			processos[i].arrival
+			);
+			for(int j=0; j< processos[i].qtdeIO; j++){
+				if (processos[i].qtdeIO-1 == j){
+					printf("%s", processos[i].io[j]);
+				}else{
+					printf("%s, ", processos[i].io[j]);
+				}
+			}
+			if (processos[i].qtdeIO == 0){
+				printf("-\n");
+			}
+			else if (numeroDeProcessos-1 == i){
+				printf("\n");
+			}
+			printf("-----------------------------\n");
+		}
+
+	printf("Tempos de Espera: \n");
+	for(int i=0; i< numeroDeProcessos; i++){
+			printf("P%d = %dms\n", processos[i].processNumber, processos[i].waitingTime);
+		}
+	
+	float divisao = (totalDeEspera / numeroDeProcessos);
+	printf("\nTempo de Espera Medio: %.2f\n", divisao);
 }
 
